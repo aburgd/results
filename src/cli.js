f273d57713c38a74b3215d282da2380a56d2ba3e
Alec Burgdorf <aeburgd@gmail.com>
2018-05-05 14:45:16 -0500

    add results function, state type
    state is used for creating a message and it's resulting boolean

---
+++ src/cli.js
@@ -0,0 +1,23 @@
+// @flow
+type state = {
+  message: string,
+  bool: boolean
+}
+
+export function results (title: string, ...statements: Array<state>) {
+  console.log(title.toUpperCase())
+  const titleLength: number = title.length
+  let div: string = ''
+  for (let i = 0; i <= titleLength; i++) {
+    div += '='
+  }
+  console.log(div)
+  statements.forEach((statement: state) => {
+    let message: string = statement.message
+    let bool: boolean = statement.bool
+
+    let outMsg = message + ': ' + bool.toString()
+    console.log(outMsg)
+  })
+  console.log('\n')
+}
